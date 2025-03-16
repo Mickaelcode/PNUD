8072df471ef3e9567b5813f92ba7c653d43beccb
Mickael Joseph <mickaelandriana06@gmail.com>
2025-03-16 03:45:14 +0300

    remove file

--- backPostgresql/models/utilisateur.js
+++ backPostgresql/models/utilisateur.js
@@ -1,15 +1,12 @@
 const {Db} = require('../BD/db')
 /**
- * matricule,name,firstName,fonction,type,password
+ * matricule,name,firstName,fonction,role,password
  */
 class Utilisateur{
-    constructor(matricule,name,lastName,fonction, mdp, type){
+    constructor(matricule, mdp, role){
         this.matricule=matricule
         this.mdp=mdp
-        this.type = type
-        this.fonction = fonction
-        this.lastName = lastName
-        this.name = name
+        this.role = role
         this.db=new Db
     }
 
@@ -18,22 +15,13 @@ class Utilisateur{
 
     async connection(){
         try {
-            return await this.db.selectByConstraint(this.type,['matricule','mot_de_passe'],[this.matricule,this.mdp],'=','&')!=0
+            return await this.db.selectByConstraint(this.role,['matricule','mot_de_passe'],[this.matricule,this.mdp],'=','&')!=0
         } catch (err) {
             console.error('Error executing selectByConstraint query:', err.message);
             throw err;
         }
     }
 
-    async update(){
-        try{
-            return await this.db.update(this.type,['matricule','mot_de_passe'],[this.matricule,this.mdp],this.matricule,this.mdp)
-        }catch{
-            console.log('Error updating query',err.message);
-            throw err 
-            
-        }
-    }
 }
 
 module.exports = {Utilisateur}
