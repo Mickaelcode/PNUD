3faa8e6d9a6703da19d17679a5341a550c6c6b4b
Mickael Joseph <mickaelandriana06@gmail.com>
2025-03-16 03:30:44 +0300

    add update methode to user model

--- backPostgresql/models/utilisateur.js
+++ backPostgresql/models/utilisateur.js
@@ -1,19 +1,37 @@
 const {Db} = require('../BD/db')
-
+/**
+ * matricule,name,firstName,fonction,type,password
+ */
 class Utilisateur{
-    constructor(matricule, mdp, role){
+    constructor(matricule,name,lastName,fonction, mdp, type){
         this.matricule=matricule
         this.mdp=mdp
-        this.role=role
+        this.type = type
+        this.fonction = fonction
+        this.lastName = lastName
+        this.name = name
         this.db=new Db
     }
 
+
+
+
     async connection(){
         try {
-            return await this.db.selectByConstraint(this.role,['matricule','mot_de_passe'],[this.matricule,this.mdp],'=','&')!=0
+            return await this.db.selectByConstraint(this.type,['matricule','mot_de_passe'],[this.matricule,this.mdp],'=','&')!=0
         } catch (err) {
             console.error('Error executing selectByConstraint query:', err.message);
             throw err;
+        }
+    }
+
+    async update(){
+        try{
+            return await this.db.update(this.type,['matricule','mot_de_passe'],[this.matricule,this.mdp],this.matricule,this.mdp)
+        }catch{
+            console.log('Error updating query',err.message);
+            throw err 
+            
         }
     }
 }
